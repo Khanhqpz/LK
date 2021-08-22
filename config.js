1c2f18107c4dc785110ddd0da1dd6e8f6f915cdd
Khanhqpz <89333361+Khanhqpz@users.noreply.github.com>
2021-08-22 14:47:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,15 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'Hí bạn',
+    introDesc: `Khảo sát tý thôi mà nhớ là trả lời thật lòng nha =))`,
+    btnIntro: 'Ok nè',
+    title: 'Bạn có yêu quý Khánh không nè :33',
+    desc: 'Chọn đi',
+    btnYes: 'Có nèeee hiiii',
+    btnNo: 'Không, Bạn bị ảo ak ???',
+    question:'Sau 4 năm học bạn thấy Khánh là người ntn nhỉ :333',
+    btnReply: 'Gửi nèee <3',
+    reply: 'Khánh cũng dễ mến này, đẹp trai, tốt bụng nữa <33333333',
+    mess: 'Khánh biết mà. Không ngờ bạn nói thế á. Ngại waa :3',
+    messDesc: 'Lúc nào sn Khánh nhớ tặng quá nhéeeee',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
